e9e0ada8d5d652f38575bd07c7424100debe8f98
mfreema1 <mfreema1@stevens.edu>
2018-11-19 14:55:51 -0500

    Add initial mongoose hooks

--- src/models/Album.js
+++ src/models/Album.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
+const Song = require('./Song');
+const Band = require('./Band');
 
 const albumSchema = new mongoose.Schema({
     band: {
@@ -18,6 +20,26 @@ const albumSchema = new mongoose.Schema({
     genre: {
         type: String
     }
+});
+
+//check if it is new or an update, act accordingly
+//NOTE: CANNOT USE ARROW FUNCTIONS DUE TO 'THIS'
+albumSchema.pre('save', async function() {
+    if(this.isNew) {
+        //it's new, add to band list
+        await Band.updateOne({_id: this.band }, { $addToSet: { albums: this._id }});
+    }
+    else {
+        //update, check what is modified using isModified
+    }
+});
+
+//this references the object called on
+albumSchema.pre('remove', async function() {
+    //remove all songs
+    await Song.deleteMany({ _id: { $in: this.songs }});
+    //remove from band album list
+    await Band.updateOne({ _id: this.bandId }, { $pull: { albums: this._id }});
 });
 
 module.exports = mongoose.model('Album', albumSchema)
--- src/models/Song.js
+++ src/models/Song.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId;
+const Album = require('./Album');
 
 const songSchema = new mongoose.Schema({
     album: {
@@ -22,6 +23,11 @@ const songSchema = new mongoose.Schema({
             validator: (v) => { return v >= 0 }
         }
     }
+});
+
+//If the song is new, append to the album song set
+songSchema.pre('save', async function() {
+    if(this.isNew) await Album.updateOne({ _id: this.album },  { $addToSet: { songs: this._id }});
 });
 
 module.exports = mongoose.model('Song', songSchema)
