371827cc6560aa28b0a590cc69cbe5d1f2cabe24
mfreema1 <mfreema1@stevens.edu>
2018-11-19 22:25:10 -0500

    Typo on jest file

--- tests/insertBand.test.js
+++ tests/insertBand.test.js
@@ -4,9 +4,10 @@ const seedData = require('../utils/seed_data.json');
 const conn = require('../src/db');
 
 test('Simple insert 1 band',  async () => {
+    await conn.dropDatabase();
     const firstBand = seedData.Bands[0];
     const entry = await ops.insertBand(firstBand);
     const res = await bands.findById(entry._id)
     expect(res.name).toBe(firstBand.name);
-    conn.disconnect();
+    await conn.disconnect();
 });
