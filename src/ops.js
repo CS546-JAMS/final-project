27afcc7e87ba7a6e851288e4e8ad2e97175f2a99
mfreema1 <mfreema1@stevens.edu>
2018-11-23 20:42:10 -0500

    Add new db operations

--- src/ops.js
+++ src/ops.js
@@ -4,11 +4,32 @@ const insert = async (schema, params) => {
     return doc;
 }
 
-const remove = async (schema, id) => {
-    schema.findById(id).then((doc) => { doc.remove(); });
+const insertMany = async (schema, arr) => {
+    //if we want the hooks, we can't use Model.collection.insert,
+    //we must iterate and generate models as we go
+
+    //iterating over a series of async calls can be tricky, as it will want to
+    //return when not all are done.  Can use a closure with a simple counter to keep track
+    let promises = [];
+    arr.forEach(async (params) => {
+        const doc = new schema(params);
+        promises.push(doc.save());
+    });
+    return Promise.all(promises);
+}
+
+const removeById = async (schema, id) => {
+    const doc = await schema.findById(id)
+    await doc.remove();
+}
+
+const retrieveAll = async (schema) => {
+    return await schema.find({});
 }
 
 module.exports = {
     insert: insert,
-    remove: remove
+    insertMany: insertMany,
+    removeById: removeById,
+    retrieveAll: retrieveAll
 }
