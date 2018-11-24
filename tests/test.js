65a9601d797a419ef7ae040ca4e54510aef0ace4
mfreema1 <mfreema1@stevens.edu>
2018-11-23 20:41:25 -0500

    Add db dump for examination

--- tests/test.js
+++ tests/test.js
@@ -5,11 +5,21 @@ const seedData = require('../utils/seed_data.json');
 const ops = require('../src/ops');
 const conn = require('../src/db');
 
+const dumpDb = async () => {
+    const fs = require('fs');
+    let out = {};
+    out.Songs = await ops.retrieveAll(Song);
+    out.Albums = await ops.retrieveAll(Album);
+    out.Bands = await ops.retrieveAll(Band);
+    fs.writeFile('out.json', JSON.stringify(out, null, 4)); //pretty print out file
+}
+
 beforeEach(() => {
     conn.dropDatabase();
 });
 
-afterAll(() => {
+afterAll(async () => {
+    //await dumpDb();
     conn.disconnect();
 });
 
@@ -22,7 +32,7 @@ test('Simple insert 1 band', async () => {
 test('Delete a band, resolve dependencies', async () => {
     let params;
     params = seedData.Bands[0];
-    const bandEntry = await ops.insert(Band, params);
+    let bandEntry = await ops.insert(Band, params);
 
     params = seedData.Albums[0];
     params.band = bandEntry._id;
@@ -32,10 +42,37 @@ test('Delete a band, resolve dependencies', async () => {
     params.album = albumEntry._id;
     const songEntry = await ops.insert(Song, params);
 
-    const bandAfterAdditions = await Band.findById(bandEntry._id);
-    await bandAfterAdditions.remove();
+    bandEntry = await Band.findById(bandEntry._id);
+    await bandEntry.remove();
 
     //assure that the references to the album and the song were deleted by the band hooks
     expect(await Album.findById(albumEntry._id)).toBe(null);
     expect(await Song.findById(songEntry._id)).toBe(null);
+});
+
+test('Make sure an album deletion only affects its songs', async () => {
+    let params;
+    params = seedData.Bands[0];
+    const bandEntry = await ops.insert(Band, params);
+
+    params = seedData.Albums[0];
+    params.band = bandEntry._id;
+    let firstAlbum = await ops.insert(Album, params);
+
+    params = seedData.Albums[1];
+    params.band = bandEntry._id;
+    let secondAlbum = await ops.insert(Album, params);
+
+    params = [seedData.Songs[0], seedData.Songs[1], seedData.Songs[2]]
+    params.forEach((s) => { s.album = firstAlbum._id });
+    await ops.insertMany(Song, params);
+
+    params = [seedData.Songs[3], seedData.Songs[4], seedData.Songs[5], seedData.Songs[6]];
+    params.forEach((s) => { s.album = secondAlbum._id });
+    await ops.insertMany(Song, params);
+
+    await ops.removeById(Album, firstAlbum._id);
+    const res = await Song.find({ album: secondAlbum._id });
+    expect(res.length).toBe(4);
+    expect(await Song.find({ album: firstAlbum._id })).toEqual([]); //object equality because find() must return array
 });
