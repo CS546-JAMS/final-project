6123dd4aa627f287df5b749ee49ea4be479f62ed
mfreema1 <mfreema1@stevens.edu>
2018-11-26 15:54:45 -0500

    Fix genre updating on removes and updates

--- src/models/Album.js
+++ src/models/Album.js
@@ -28,14 +28,14 @@ albumSchema.pre('save', async function() {
         await mongoose.model('Band').updateOne({ _id: this.band }, { $addToSet: { albums: this._id, genres: this.genre }});
     }
     else if(this.modifiedPaths().includes('genre')) {
-        const album = await mongoose.model('Album').findById(this._id); //old genre
-        //consider replacing with { 'Classic Rock': 2, 'Metal': 1 } and decrementing / incrementing
+        //handle the removal of the old genre
+        //consider replacing with { 'Classic Rock': 2, 'Metal': 1 } on band and decrementing / incrementing 
         //to avoid having to query the whole set
-        const sameGenre = await mongoose.model('Album').find({ genre: album.genre });
-        if (!sameGenre || sameGenre.length < 2) //we don't have another of the same, pull it from the genres
-            await mongoose.model('Band').updateOne({ _id: this.band }, { $addToSet: { genres: this.genre }, $pull: { genres: album.genre }});
-        else //we have another, don't pull
+        const sameGenre = await mongoose.model('Album').find({ band: this.band, genre: this._previousGenre });
             await mongoose.model('Band').updateOne({ _id: this.band }, { $addToSet: { genres: this.genre }});
+        if (sameGenre.length < 2) { //we don't have another of the same, pull it from the 
+            await mongoose.model('Band').updateOne({ _id: this.band }, { $pull: { genres: this._previousGenre }});
+        }
     }
 });
 
