1c9be55b516ed7c929506da5015c1127d230c7c4
mfreema1 <mfreema1@stevens.edu>
2018-11-26 15:52:19 -0500

    Remove db connection logs for now

--- src/db.js
+++ src/db.js
@@ -11,27 +11,27 @@ class Database {
     _connect() {
         mongoose.connect(`mongodb://${server}/${db}`, { useCreateIndex: true, useNewUrlParser: true })
             .then(() => {
-                console.log('Db connection successful');
+                //console.log('Db connection successful');
             })
             .catch((err) => {
-                console.log('Db connection failed');
+                //console.log('Db connection failed');
             });
     }
 
     dropDatabase() {
         return mongoose.connection.dropDatabase()
             .then(() => { 
-                console.log('Db dropped');
+                //console.log('Db dropped');
             })
             .catch((err) => {
-                console.log('Db failed to drop');
+                //console.log('Db failed to drop');
             });
     }
 
     disconnect() {
         return mongoose.disconnect()
             .then(() => { 
-                console.log('Bye!'); 
+                //console.log('Bye!'); 
             });
     }
 };
