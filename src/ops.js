29abf83a2e9cdf5f030c784fae92c8ab13d8e895
mfreema1 <mfreema1@stevens.edu>
2018-11-29 13:46:43 -0500

    Add new operations

--- src/ops.js
+++ src/ops.js
@@ -5,13 +5,20 @@ const models = {
     'artist': require('./models/Artist')
 }
 
+const _assureSchema = (schema) => {
+    if(!schema) throw 'Schema undefined or null';
+    if(!models[schema]) throw `No schema found for: ${schema}`;
+}
+
 const insert = async (schema, params) => {
+    _assureSchema(schema)
     const doc = new models[schema](params);
     await doc.save();
     return doc;
 }
 
 const insertMany = async (schema, arr) => {
+    _assureSchema(schema)
     //if we want the hooks, we can't use Model.collection.insert,
     //we must iterate and generate models as we go
 
@@ -26,24 +33,37 @@ const insertMany = async (schema, arr) => {
 }
 
 const removeById = async (schema, id) => {
+    _assureSchema(schema)
     const doc = await models[schema].findById(id)
     await doc.remove();
 }
 
 const getAll = async (schema) => {
+    _assureSchema(schema)
     return await models[schema].find({});
 }
 
 const getById = async (schema, id) => {
+    _assureSchema(schema)
     return await models[schema].findById(id);
 }
 
 const getByParams = async (schema, params) => {
+    _assureSchema(schema)
     return await models[schema].find(params);
 }
 
-const update = async (doc, params) => {
+const updateByDoc = async (doc, params) => {
+    doc.set(params);
+    await doc.save();
+}
+
+const updateById = async (schema, id, params) => {
+    _assureSchema(schema)
+    const doc = await models[schema].findById(id);
+    console.log(doc);
     doc.set(params); //need to inform mongoose of changes using this or manually using markModified()
+    console.log(doc);
     await doc.save();
 }
 
@@ -54,5 +74,6 @@ module.exports = {
     getAll: getAll,
     getById: getById,
     getByParams: getByParams,
-    update: update
+    updateByDoc: updateByDoc,
+    updateById: updateById
 }
