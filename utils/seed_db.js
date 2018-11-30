7e1d508c2892096bc49a2159cf3e1dfabad6670e
mfreema1 <mfreema1@stevens.edu>
2018-11-30 11:27:08 -0500

    Update seed script

--- utils/seed_db.js
+++ utils/seed_db.js
@@ -1,42 +1,43 @@
-const seedData = require('./seed_data');
-const conn = require('../src/db');
-const Song = require('../src/models/Song');
-const Album = require('../src/models/Album');
-const Band = require('../src/models/Band');
 const ops = require('../src/ops');
+const conn = require('../src/db');
+const seedData = require('./seed_data');
 
 const main = async () => {
-    //drop everything
-    conn.dropDatabase();
-    console.log('Database dropped');
+    await conn.dropDatabase();
 
-    //repopulate everything
     let params;
     params = seedData.Bands[0];
-    const gunsNRoses = await ops.insert(Band, params);
+    const gunsNRoses = await ops.insert('band', params);
 
     params = seedData.Albums[0];
     params.band = gunsNRoses._id;
-    const appetiteForDestruction = await ops.insert(Album, params);
+    const appetiteForDestruction = await ops.insert('album', params);
 
-    params = seedData.Songs[0];
-    params.album = appetiteForDestruction._id;
-    const paradiseCity = await ops.insert(Song, params);
+    params = seedData.Albums[1];
+    params.band = gunsNRoses._id;
+    const useYourIllusion = await ops.insert('album', params);
 
-    params = seedData.Songs[1];
-    params.album = appetiteForDestruction._id;
-    const welcomeToTheJungle = await ops.insert(Song, params);
+    let promises = []
+    params = seedData.Artists;
+    params.forEach((a) => { a.bands[0].band = gunsNRoses._id });
+    promises.push(ops.insertMany('artist', params));
 
-    console.log('Seed db generated');
-    console.log('Band======')
-    console.log(await Band.findOne({ name: "Guns N' Roses" }));
-    console.log('Album======')
-    console.log(await Album.findOne({ title: "Appetite for Destruction" }));
+    params = seedData.Songs.slice(0, 3);
+    params.forEach((s) => { s.album = appetiteForDestruction._id });
+    promises.push(ops.insertMany('song', params));
 
-    await ops.removeById(Song, paradiseCity._id);
-    console.log(await Album.findOne({ title: "Appetite for Destruction" }));
-    await ops.removeById(Song, welcomeToTheJungle._id);
-    console.log(await Album.findOne({ title: "Appetite for Destruction" }));
+    params = seedData.Songs.slice(3);
+    params.forEach((s) => { s.album = useYourIllusion._id });
+    promises.push(ops.insertMany('song', params));
+
+    return Promise.all(promises);
 };
 
-main();
+main()
+    .then(() => { 
+        return conn.dumpDb()
+    })
+    .then(() => { 
+        return conn.disconnect() 
+    }); 
+    
