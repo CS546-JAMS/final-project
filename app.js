171f516e35a876f426924eb6bd635fb0ede9e0e2
mfreema1 <mfreema1@stevens.edu>
2018-12-06 11:19:06 -0500

    Add helper

--- app.js
+++ app.js
@@ -4,11 +4,22 @@ const handlebars = require("express-handlebars");
 const port = 3000;
 const app = express();
 
+//object represents the arguments packaged up
+const hbs = handlebars.create({
+    helpers: {
+        link: (type, id, text) => {
+            return `<a href="/${type}/${id}">${text}</a>`
+        }
+    },
+    defaultLayout: "main",
+    extname: ".hbs"
+})
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); //Remains to be seen if we'll be receiving json
-app.engine("handlebars", handlebars({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
 
 require('./routes')(app);
 
