37e8f627d91eed5053179d87e3349a5422323ba7
mfreema1 <mfreema1@stevens.edu>
2018-12-13 15:42:56 -0500

    New helpers

--- helpers.js
+++ helpers.js
@@ -38,12 +38,43 @@ module.exports = {
         return num.toLocaleString()
     },
 
-    linkList: (type, params, grey = false) => {
-        return params.map((pair) => {
+    //TODO: simplify, this is way too complex, very repetitive
+    linkList: (type, lst, grey = false) => {
+        //handle simple arrays (for genres where name and id are the same)
+        //i.e. ["Rock", "Classic Rock"]
+        if(lst.length > 0 && typeof lst[0] !== "object") {
+            return lst.map((name) => {
+                const url = encodeURI(`/${type}/${name}`)
+                if(grey)
+                    return `<a class="greyed-text" href="${url}">${name}</a>`
+                return `<a href="${url}">${name}</a>`
+            })
+        }
+        //handle an object-arry like [{id: foo, name: bar}]
+        return lst.map((pair) => {
+            pair.name = pair.name || pair.title //it's always a name or title, never both
             const url = encodeURI(`/${type}/${pair._id}`) //TODO: see about extracting this, similar functionality to link
             if(grey)
                 return `<a class="greyed-text" href="${url}">${pair.name}</a>`
             return `<a href="${url}">${pair.name}</a>`
         })
+    },
+
+    timeFrame: (id, lst) => {
+        let yearStart, yearEnd;
+        for(let history of lst) {
+            if(history.band.toString() == id.toString()) { //they're technically objects (Object ID)
+                yearStart = history.yearStart
+                yearEnd = history.yearEnd
+                break
+            }
+        }
+
+        if(yearStart && yearEnd)
+            return `(${yearStart} - ${yearEnd})`
+        if(yearStart && !yearEnd)
+            return `(${yearStart} - Present)`
+        if(!yearStart && yearEnd)
+            return `(Until ${yearEnd})`
     }
 }
