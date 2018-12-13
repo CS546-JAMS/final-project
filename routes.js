7feb4d7f2c07a56ee050c1d5d1908235725325e7
JHobbie <jaredhobbie1@gmail.com>
2018-12-13 16:31:51 -0500

    Attempt to add Genre Details Page
    Mad ugly tho

--- routes.js
+++ routes.js
@@ -107,11 +107,11 @@ module.exports = app => {
     app.get('/genres/:name', (req, res) => {
         //return a list of the most popular bands in that genre
         mongoose.model('Genre').find({ title: req.params.name })
-            .populate('bands', 'name likes') //retrieve only the band's name and likes
+            .populate('bands', 'name albums likes') //retrieve only the band's name and likes
             .sort('bands.likes')
             .limit(10)
-            .then((bands) => {
-                res.status(200).send(bands);
+            .then((genres) => {
+                res.render('layouts/genreDetails', {genres});
             })
             .catch((err) => handleErr(err, res));
     });
@@ -310,7 +310,7 @@ module.exports = app => {
                     layout: 'genreDetails',
                     search: (title) => { 
                         return mongoose.model('Genre').findOne({ title })
-                            .populate('bands', 'name likes') //retrieve only the band's name and likes
+                            .populate('bands', 'name albums likes') //retrieve only the band's name and likes
                             .sort('bands.likes')
                             .limit(10)
                     }
